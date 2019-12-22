15d95c1e098893434eda0aa7ea3952a7065b8e0f
Janice Bradford <janice.bradford@outlook.com>
2019-12-21 18:06:18 -0800

    Design Product tooltip is working.

--- src/tooltips.js
+++ src/tooltips.js
@@ -1,5 +1,6 @@
-console.log("Hello Project.");
+// console.log("Hello Project.");
 
+// Design
 let designicon = document.getElementById("designicon");
 let designtooltip = document.getElementById("designtooltip");
 
@@ -8,11 +9,21 @@ designicon.addEventListener("mouseout", hideTooltip);
 
 function showTooltip(event) {
   let designiconPos = designicon.getBoundingClientRect();
-  designtooltip.style.left = designiconPos.right + 20 + "px";
-  designtooltip.style.top = window.scrollY + designiconPos - 60 + "px";
+  designtooltip.style.left = designiconPos.right + 10 + "px";
+  designtooltip.style.top = window.scrollY + designiconPos.top - 30 + "px";
   designtooltip.style.display = "block";
 }
 
 function hideTooltip(event) {
   designtooltip.style.display = "none";
 }
+
+// Packaging
+let packagingicon = document.getElementById("packagingicon");
+let packagingtooltip = document.getElementById("packagingtooltip");
+
+// Manufacture
+
+// Warehouse
+
+// Headquarters
