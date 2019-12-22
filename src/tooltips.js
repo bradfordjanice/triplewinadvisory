a1d8d5187d81be68c44bd2289049fa8c7ec246e3
Janice Bradford <janice.bradford@outlook.com>
2019-12-21 18:20:02 -0800

    Packaging tooltip is working.

--- src/tooltips.js
+++ src/tooltips.js
@@ -4,23 +4,38 @@
 let designicon = document.getElementById("designicon");
 let designtooltip = document.getElementById("designtooltip");
 
-designicon.addEventListener("mouseover", showTooltip);
-designicon.addEventListener("mouseout", hideTooltip);
+designicon.addEventListener("mouseover", showDesignTooltip);
+designicon.addEventListener("mouseout", hideDesignTooltip);
 
-function showTooltip(event) {
+function showDesignTooltip(event) {
   let designiconPos = designicon.getBoundingClientRect();
   designtooltip.style.left = designiconPos.right + 10 + "px";
   designtooltip.style.top = window.scrollY + designiconPos.top - 30 + "px";
   designtooltip.style.display = "block";
 }
 
-function hideTooltip(event) {
+function hideDesignTooltip(event) {
   designtooltip.style.display = "none";
 }
 
 // Packaging
 let packagingicon = document.getElementById("packagingicon");
 let packagingtooltip = document.getElementById("packagingtooltip");
+
+packagingicon.addEventListener("mouseover", showPackagingTooltip);
+packagingicon.addEventListener("mouseout", hidePackagingTooltip);
+
+function showPackagingTooltip(event) {
+  let packagingiconPos = packagingicon.getBoundingClientRect();
+  packagingtooltip.style.left = packagingiconPos.right + 10 + "px";
+  packagingtooltip.style.top =
+    window.scrollY + packagingiconPos.top - 30 + "px";
+  packagingtooltip.style.display = "block";
+}
+
+function hidePackagingTooltip(event) {
+  packagingtooltip.style.display = "none";
+}
 
 // Manufacture
 
